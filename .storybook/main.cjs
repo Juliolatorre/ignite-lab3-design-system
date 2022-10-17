c37ead722f1e6a40ccd713d6e395954c60b90509
Juliolatorre <julio.c.latorre@gmail.com>
2022-10-17 15:50:13 -0300

    ci: Use legacy peer deps

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab3-design-system/'
+    }
+
+    return config
   }
 }
